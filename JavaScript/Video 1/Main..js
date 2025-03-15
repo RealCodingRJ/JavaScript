7ee74edc8cf4ecdf69089996428eac23a52d853e
Ryan James <realcoder2023@gmail.com>
2025-03-14 21:51:45 -0400

    added day one

---
+++ JavaScript/Video 1/Main..js
@@ -0,0 +1,35 @@
+var number = 5; // Single Line Comment
+
+var a = 5;
+var b = 2;
+var c = "I am ";
+
+a = a + 1;
+b = b + 5;
+c = c + "a String";
+
+console.log(a, b, c);
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 56;
+properCamelCase = "a Person";
+titleCaseOver = 9000;
+
+var addNum = 2 + 9;
+var sub = 20 - 9;
+var mul = 10 * 3;
+var div = mul / 2;
+
+console.log("div:", div);
+
+var inc = 10;
+inc += 1;
+
+var dec = 4;
+dec -= 1;
+
+console.log("inc:", inc);
+console.log("dec:", dec);
